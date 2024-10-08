f0d49aaa022dd2185e7c85c33a0713ffcd198b5b
MattJ12344 <122521449+MattJ12344@users.noreply.github.com>
2024-10-08 10:55:30 +0200

    Create 383. Ransom Note.js

---
+++ 383. Ransom Note.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    let magaHash = {};
+
+    for (let c of magazine) {
+        magaHash[c] = (magaHash[c] || 0) + 1;
+    }
+
+    for (let c of ransomNote) {
+        if (!magaHash[c] || magaHash[c] <= 0) {
+            return false;
+        }
+        magaHash[c]--;
+    }
+    
+    return true;    
+};
